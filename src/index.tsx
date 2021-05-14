fa70301b22d6742ff2f0ead6da74dad6d6fa4c33
jhdcruz <jhdcruz00@gmail.com>
2021-05-14 22:29:08 +0800

    ui: Changed font to 'Inter'
    Added svg background

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,9 @@ const GlobalTheme = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    background-color: #fff3d5;
-    font-family: -apple-system, BlinkMacSystemFont, 'Open Sans', sans-serif;
+    background: #fff3d5 url("https://gist.githubusercontent.com/jhdcruz/f00e47427a1d71f7f70211c0ac32ce4e/raw/c1cdf2365f8b719710098e0c309ea858824f0aa9/bgpattern.svg") center center;
+    background-size: cover;
+    font-family: -apple-system, BlinkMacSystemFont, 'Inter', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,19 +1,11 @@
 import { DefaultTheme } from "styled-components";
 
 const fonts = {
-  header: "-apple-system, BlinkMacSystemFont, 'Russo One', sans-serif",
-  body: "-apple-system, BlinkMacSystemFont, 'Open Sans', sans-serif",
-  shadow: "0 2px 5px #000",
+  header: "-apple-system, BlinkMacSystemFont, 'Inter', sans-serif",
 };
 
 const colors = {
   primary: "#fff3d5",
-  secondary: "blue",
-  tertiary: "grey",
-  success: "green",
-  danger: "red",
-  warning: "yellow",
-  info: "cyan",
   light: "#fefae0",
   dark: "#0e0e0e",
 };
