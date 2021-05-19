05154f7c6747d0da677d1e5b342d333c50666d07
jhdcruz <jhdcruz00@gmail.com>
2021-05-19 22:30:35 +0800

    ui: Gallery boilerplate w/ placeholder

---
+++ src/components/Art.tsx
@@ -0,0 +1,61 @@
+import { ReactNode } from "react";
+import styled from "styled-components";
+
+const Panel = styled.article`
+  margin: 7vw 0;
+  color: ${(props) => props.theme.colors.dark};
+  font-size: 1.5vw;
+  height: max-content;
+  word-wrap: break-word;
+  vertical-align: middle;
+  position: relative;
+`;
+
+const Image = styled.img`
+  margin: 0 auto;
+  display: block;
+  width: 90%;
+  height: auto;
+  border-radius: 0.5vw;
+  box-shadow: 0 0.15vw 0.7vw #777;
+`;
+
+const About = styled.p`
+  color: ${(props) => props.theme.colors.dark};
+  background-color: rgba(255, 243, 213, 0.4);
+  margin: 0 3vw;
+  padding: 1.5vw 3vw;
+  font-size: 1.25vw;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  border-bottom-left-radius: 0.5vw;
+  border-bottom-right-radius: 0.5vw;
+`;
+
+const Title = styled.h1`
+  font-weight: 900;
+  font-size: 1.8vw;
+  line-height: 1 !important;
+  margin: 1vw 0;
+  text-transform: uppercase;
+`;
+
+export type Content = {
+  img?: string;
+  title?: string;
+  desc?: string | ReactNode;
+};
+
+const Art = ({ img, title, desc }: Content) => (
+  <Panel>
+    <Image src={img} alt={title} draggable="false" />
+    <About>
+      <Title>{title}</Title>
+      {desc}
+    </About>
+  </Panel>
+);
+
+export default Art;
--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,3 +1,50 @@
-const Gallery = () => <>test2</>;
+import styled from "styled-components";
+import Art, { Content } from "../components/Art";
+import art1 from "../assets/img/placeholder.jpg";
+
+// TODO: Arts could be dynamically imported.
+//       https://loadable-components.com/docs/dynamic-import/
+
+const Showcase = styled.section`
+  display: block;
+  overflow: auto;
+  height: 55vw;
+`;
+
+const arts = [
+  {
+    img: `${art1}`,
+    title: "Sample Art Title",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum mi et nibh sodales iaculis. Praesent consectetur feugiat convallis. Proin eget sagittis massa.",
+  },
+  {
+    img: `${art1}`,
+    title: "Sample Art Title",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum mi et nibh sodales iaculis. Praesent consectetur feugiat convallis. Proin eget sagittis massa.",
+  },
+  {
+    img: `${art1}`,
+    title: "Sample Art Title",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum mi et nibh sodales iaculis. Praesent consectetur feugiat convallis. Proin eget sagittis massa.",
+  },
+  {
+    img: `${art1}`,
+    title: "Sample Art Title",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum mi et nibh sodales iaculis. Praesent consectetur feugiat convallis. Proin eget sagittis massa.",
+  },
+  {
+    img: `${art1}`,
+    title: "Sample Art Title",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum mi et nibh sodales iaculis. Praesent consectetur feugiat convallis. Proin eget sagittis massa.",
+  },
+];
+
+const Gallery = () => (
+  <Showcase>
+    {arts.map(({ img, title, desc }: Content) => (
+      <Art img={img} title={title} desc={desc} />
+    ))}
+  </Showcase>
+);
 
 export default Gallery;
