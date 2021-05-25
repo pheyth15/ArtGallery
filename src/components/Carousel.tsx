c94d49ab0674439fcafa6c8cd9fff08303d8851e
jhdcruz <jhdcruz00@gmail.com>
2021-05-25 21:02:14 +0800

    refactor: Lazy-load Carousel component

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,7 +1,11 @@
+import { lazy, Suspense } from "react";
 import { createGlobalStyle } from "styled-components";
-import ImageGallery from "react-image-gallery";
 import { CarouselProps } from "../types";
+import Loader from "./Loader";
+
 import "react-image-gallery/styles/scss/image-gallery.scss";
+
+const ImageGallery = lazy(() => import("react-image-gallery"));
 
 const ImageCarousel = createGlobalStyle`
   .image-gallery {
@@ -14,22 +18,30 @@ const ImageCarousel = createGlobalStyle`
       border-radius: 0.5vw;
       box-shadow: 0 0.15vw 0.7vw #777;
     }
+
+    .image-gallery-thumbnails {
+      overflow-y: auto;
+
+      ::-webkit-scrollbar {
+        width: 0px;
+        background: transparent; /* make scrollbar transparent */
+      }
+    }
   }
 `;
 
 const Carousel = ({ screens }: CarouselProps) => (
-  <>
+  <Suspense fallback={<Loader />}>
     <ImageCarousel />
     <ImageGallery
       items={screens}
-      lazyLoad={true}
       autoPlay={true}
       slideInterval={10000}
       thumbnailPosition="left"
       disableThumbnailScroll={true}
       showPlayButton={false}
     />
-  </>
+  </Suspense>
 );
 
 export default Carousel;
--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,25 +1,16 @@
 import styled from "styled-components";
 import Loading from "../assets/img/loading-tr.gif";
 
-const Constraint = styled.div`
-  width: 100%;
-  height: 100vh;
-`;
-
 const PageLoader = styled.img`
-  width: 200px;
+  width: 12vw;
   height: auto;
   position: absolute;
   top: 40%;
   left: 50%;
   transform: translate(-50%);
-  z-index: 10;
+  z-index: 99;
 `;
 
-const Loader = () => (
-  <Constraint>
-    <PageLoader src={Loading} alt="Website loading..." />
-  </Constraint>
-);
+const Loader = () => <PageLoader src={Loading} alt="Website loading..." />;
 
 export default Loader;
